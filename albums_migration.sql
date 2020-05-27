d0b826450c1d4202309920e3b86f8f9378d16de1
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-27 07:49:24 -0500

    Added mysql table

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    album_name  VARCHAR(100) NOT NULL,
+    release_date int NOT NULL,
+    sales DECIMAL(6, 3) NOT NULL,
+    genre VARCHAR (50) NOT NULL,
+    PRIMARY KEY (id)
+);
