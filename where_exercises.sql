cad271beb55b1abe4ec5336779a3976fbd99ce81
DeLayne LaBove <delayne.labove@gmail.com>
2020-05-27 15:55:18 -0500

    Last commit after walkthrough

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,20 +1,42 @@
 use employees;
 
-select *
-from employees
-WHERE first_name
-IN ('Irena', 'Vidya', 'Maya');
+# select *
+# from employees
+# WHERE first_name
+# IN ('Irena', 'Vidya', 'Maya');
 
 select *
-from employees
-where last_name
-LIKE 'E%';
+    from employees
+    WHERE first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya';
+    AND gender = 'M';
+
+
+select *
+    from employees
+    where last_name
+    LIKE 'E%'
+    OR last_name LIKE '%E';
+
+
+SELECT *
+    FROM employees
+    WHERE hire_date like '199%';
+
+SELECT *
+    FROM employees
+    WHERE birth_date like '%12-25';
 
 SELECT *
 FROM employees
-WHERE hire_date between '1990-01-01' and '1999-12-31';
+WHERE hire_date LIKE '199%'
+  AND birth_date LIKE '%12-25';
 
 SELECT *
-FROM employees
-WHERE birth_date like '%12-25';
+    FROM employees
+    WHERE last_name LIKE '%q%';
 
+SELECT *
+    FROM employees
+    WHERE last_name LIKE ''
