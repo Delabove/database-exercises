d60670ba1ec1edb3300edcac1edb5b8b1f3985d3
DeLayne LaBove <delayne.labove@gmail.com>
2020-06-01 07:57:11 -0500

    Joins 2

--- src/join_exercises.sql
+++ src/join_exercises.sql
@@ -10,3 +10,14 @@ FROM employees
               ON departments.dept_no = dept_manager.dept_no
 WHERE dept_manager.to_date > curdate()
 ORDER BY departments.dept_name;
+
+SELECT
+departments.dept_name as 'Department Name',
+       Concat( employees.first_name, ' ', employees.last_name) as 'Department Manager'
+FROM employees
+JOIN dept_manager
+     ON employees.emp_no = dept_manager.emp_no
+JOIN departments
+     ON departments.dept_no = dept_manager.dept_no
+WHERE dept_manager.to_date > curdate() and employees.gender = 'F'
+ORDER BY departments.dept_name;
